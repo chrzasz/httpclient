5dc7808d2d0ee8180767c064b3f9edb2ee9c29db
Greg <chrzaszczyk@gmail.com>
2019-10-16 15:45:58 +0200

    fix regex

--- src/main/java/pl/inome/httpclient/controller/CurrencyController.java
+++ src/main/java/pl/inome/httpclient/controller/CurrencyController.java
@@ -19,7 +19,7 @@ public class CurrencyController {
     }
 
     private int checkCurrency(String plnRate) {
-        if (!plnRate.isEmpty() && plnRate.matches("[0-9]{1,}[.]{1}[0-9]{2}")) {
+        if (!plnRate.isEmpty() && plnRate.matches("[0-9]{1,}[.]{0,1}[0-9]{0,2}")) {
             try {
                 BigDecimal value = BigDecimal.valueOf(Double.parseDouble(plnRate));
                 return value.compareTo(getPlnRate());
